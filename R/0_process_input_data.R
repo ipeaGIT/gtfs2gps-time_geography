675b9330eba00a4b835992b225dca67233380b77
rafapereirabr <rafa.pereira.br@gmail.com>
2022-10-21 09:33:22 -0300

    easypackages::packages + typos

--- R/0_process_input_data.R
+++ R/0_process_input_data.R
@@ -1,13 +1,15 @@
-# create data-----
+# prepare data-----
 
+
+# load libraries
 rm(list=ls())
 gc(reset = TRUE)
 install.packages('easypackages')
 easypackages::packages('geobr', 'magick', 'gtfs2gps', 
-                       'data.table', 'sf', 'mapview',
+                       'data.table', 'sf', 'mapview', 'progressr',
                        'magrittr', 'dplyr', 'ggnewscale',
-                       'ggplot2', 'httr',)
-
+                       'ggplot2', 'ggmap', 'raster', 'terra',
+                       'rayshader', 'rayrender', 'rayimage')
 
 # 1) Download Sao Paulo shapefile ------
 spo_bound <- geobr::read_municipality(3550308, simplified = FALSE)
@@ -15,9 +17,9 @@ spo_bound <- sf::st_transform(spo_bound, 4326)
 readr::write_rds(spo_bound,"data/spo_bound.rds")
 
 # 1) PREP DATA FOR SPATIAL PLOTS ------
-fix_link <- "https://github.com/ipeaGIT/gtfs2gps-time_geography/releases/download/data"
-emtu_link <- sprintf("%s/gtfs_spo_emtu_2019-06.zip",fix_link)
-sptrans_link <- sprintf("%s/gtfs_spo_sptrans_2019-06.zip",fix_link)
+fixed_link <- "https://github.com/ipeaGIT/gtfs2gps-time_geography/releases/download/data"
+emtu_link <- sprintf("%s/gtfs_spo_emtu_2019-06.zip", fixed_link)
+sptrans_link <- sprintf("%s/gtfs_spo_sptrans_2019-06.zip", fixed_link)
 dir.create("data-raw/")
 
 download.file(url = emtu_link
@@ -37,7 +39,7 @@ sp_gtfs <- gtfstools::filter_by_shape_id(sp_gtfs_raw,"423032_ida")
 
 gtfs2gps::write_gtfs(sp_gtfs,"data/gtfs_spo_emtu_2019-06_423032_ida.zip")
 
-# exporting temporal intersectins ---------------------------
+# exporting temporal intersections ---------------------------
 
 
 sp_gtfs_tmp <- gtfstools::filter_by_shape_id(sp_gtfs_raw,c("540298_ida","502070_ida"))
@@ -46,4 +48,4 @@ sp_gtfs_tmp <- gtfstools::filter_by_trip_id(sp_gtfs_tmp,c("502070_ida_2_6","5402
 gps_tmp <- gtfs2gps::gtfs2gps(sp_gtfs_tmp)
 gps_tmp <- gtfs2gps::adjust_speed(gps_tmp)
 
-readr::write_rds(x = gps_tmp,"data/emtu_intersection_gps.rds")
+readr::write_rds(x = gps_tmp, "data/emtu_intersection_gps.rds")
